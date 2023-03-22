3a4105929a78b8beffe6c6fe84da8e1aab263a68
Vipul Kodle <machome@Vipuls-MacBook-Air.local>
2023-03-22 09:09:46 +0530

    Added basic config packages and created folder structure

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
 // server.js
-const express = require('express');
+const express = require("express");
+const path = require("path");
+var cors = require("cors");
+const mongoose = require("mongoose");
+
 const app = express();
-const path = require('path');
+app.use(cors());
+require("dotenv").config();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, "./build")));
 
 // API route handler
-app.get('/api/hello', (req, res) => {
-    res.send('Hello from the API!');
-  });
+app.get("/api/hello", (req, res) => {
+  res.send("Hello from the API!");
+});
 
 // Handle other routes by serving the index.html file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'./build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "./build/index.html"));
 });
+
+// Connecting to DB
+const dbUrl = process.env.DB_URL;
+mongoose
+  .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB!"))
+  .catch((err) => console.error("Error connecting to MongoDB", err));
 
 // Start the server
 const port = process.env.PORT || 4200;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
-// Run the build command to create the production build of the React app
-// For example, if you're using create-react-app, run this command in the "my-app" directory:
-// npm run build
